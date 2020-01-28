aaa8912c659930f4f95011cf213b25dac98a62af
Martin Cserép <martincserep1@gmail.com>
2020-01-28 14:01:10 +0100

    added logout button
    ## Modifications
     - [x] Login button is added
    ## File Changes
    ### Added
    > Nothing
    ### Edited
     - package.json
     - package-lock.json
     - src/pages/Home/index.js
    ### Removed
    > Nothing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,19 +7,6 @@ import firebase from "../../config/firebase";
 
 import styles from './Header.module.scss'
 
-import Search20 from "@carbon/icons-react/lib/search/20";
-import Notification20 from "@carbon/icons-react/lib/notification/20";
-import AppSwitcher20 from "@carbon/icons-react/lib/app-switcher/20";
-import {
-  Header,
-  HeaderName,
-  HeaderGlobalAction,
-  HeaderGlobalBar,
-  HeaderNavigation,
-  HeaderMenu,
-  HeaderMenuItem
-} from "carbon-components-react/lib/components/UIShell";
-
 
 const Nav = (props) => {
   
@@ -53,32 +40,7 @@ const Nav = (props) => {
 
     return(
         <div className={styles.headerContainer}>
-            <Header aria-label="IBM Platform Name">
-            <HeaderName href="#" prefix="IBM">
-                [Platform]
-            </HeaderName>
-            <HeaderNavigation aria-label="IBM [Platform]">
-                <HeaderMenuItem href="#">Link 1</HeaderMenuItem>
-                <HeaderMenuItem href="#">Link 2</HeaderMenuItem>
-                <HeaderMenuItem href="#">Link 3</HeaderMenuItem>
-                <HeaderMenu aria-label="Link 4" menuLinkName="Link 4">
-                <HeaderMenuItem href="#">Sub-link 1</HeaderMenuItem>
-                <HeaderMenuItem href="#">Sub-link 2</HeaderMenuItem>
-                <HeaderMenuItem href="#">Sub-link 3</HeaderMenuItem>
-                </HeaderMenu>
-            </HeaderNavigation>
-            <HeaderGlobalBar>
-                <HeaderGlobalAction aria-label="Search" onClick={() => {}}>
-                <Search20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction aria-label="Notifications" onClick={() => {}}>
-                <Notification20 />
-                </HeaderGlobalAction>
-                <HeaderGlobalAction aria-label="App Switcher" onClick={() => {}}>
-                <AppSwitcher20 />
-                </HeaderGlobalAction>
-            </HeaderGlobalBar>
-            </Header>
+            {buttons}
       </div>
     )
 
