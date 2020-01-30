0e12b02465b11e57d5e851ee6067d15e816e2b91
Martin Cserép <martincserep1@gmail.com>
2020-01-30 23:07:16 +0100

    Fetching all data from firebase

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 import styles from "./Card.module.scss";
 
-import timage from '../../../assets/images/bgimage.jpg';
+import PropTypes from 'prop-types';
 
 import { FaEdit } from "react-icons/fa";
 
-const Card = () => {
+const Card = ( props ) => {
+  const { image, title } = props
     return(
       <div className={styles.panel}>
           <div className={styles.imgContainer}>
-            <img src={timage} />
+            <img src={image} />
           </div>
           <div className={styles.infoContainer}>
-            <span className={styles.title}>Lillafüredi Állami Erdei Vasút</span>
+            <span className={styles.title}>{title}</span>
             <span className={styles.icon}><FaEdit /></span>
           </div>
       </div>  
     );
+}
+
+Card.propTypes = {
+	image: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
 }
 export default Card;
